5132619ca322a0a54297e43dcc45153056180f10
CRUXsur <marcojtorrico@hotmail.com>
2022-01-28 15:14:26 -0400

    prefer const: for warning

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(new MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   //MyApp:1er widget
   //todo StatelessWidget tiene
   //un metodo build el cual recibe el
@@ -12,7 +14,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     //tengo que retornar un widget
-    return MaterialApp(
+    return const MaterialApp(
+        //para los warning subrayado azul! const
         //retornaremos un widget de material, 2do widget
         //Crtl + space => aparece una lista de propiedades
         //widgets que podemos usar..
