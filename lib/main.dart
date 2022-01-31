d7021805a0ddb329bcff2d9390c2f0b498553527
CRUXsur <marcojtorrico@hotmail.com>
2022-01-31 09:33:18 -0400

    separando la logica en varios archivos

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:marknotes/screens/home_screen.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -15,16 +17,14 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     //tengo que retornar un widget
     return const MaterialApp(
+        debugShowCheckedModeBanner: false,
         //para los warning subrayado azul! const
         //retornaremos un widget de material, 2do widget
         //Crtl + space => aparece una lista de propiedades
         //widgets que podemos usar..
         //documentacion en:
         //https://api.flutter.dev/flutter/material/MaterialApp-class.html
-        home: Center(
-      //otro widget con hijo child: Text
-      child: Text('Hola Mundo'), //3er Widget Text
-    ));
+        home: HomeScreen());
   }
   //
 }
