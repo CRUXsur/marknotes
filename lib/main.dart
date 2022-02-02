1f7921d4870db722c156bf4f65e715dc8f86b60c
CRUXsur <marcojtorrico@hotmail.com>
2022-02-02 16:06:26 -0400

    Navegar a una nueva pantalla

--- lib/main.dart
+++ lib/main.dart
@@ -10,17 +10,23 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Material App',
-        //home: Listview1Screen());
-        //home: const Listview2Screen(),
-        initialRoute: 'home',
-        routes: {
-          'home': (BuildContext context) => const HomeScreen(),
-          'listview1': (BuildContext context) => const Listview1Screen(),
-          'listview2': (BuildContext context) => const Listview2Screen(),
-          'alert': (BuildContext context) => const AlertScreen(),
-          'card': (BuildContext context) => const CardScreen()
-        });
+      debugShowCheckedModeBanner: false,
+      title: 'Material App',
+      //home: Listview1Screen());
+      //home: const Listview2Screen(),
+      initialRoute: 'home',
+      routes: {
+        'home': (BuildContext context) => const HomeScreen(),
+        'listview1': (BuildContext context) => const Listview1Screen(),
+        'listview2': (BuildContext context) => const Listview2Screen(),
+        'alert': (BuildContext context) => const AlertScreen(),
+        'card': (BuildContext context) => const CardScreen()
+      },
+      onGenerateRoute: (settings) {
+        return MaterialPageRoute(
+          builder: (context) => const AlertScreen(),
+        );
+      },
+    );
   }
 }
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:marknotes/screens/screens.dart';
+
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
@@ -15,7 +17,13 @@ class HomeScreen extends StatelessWidget {
                   leading: const Icon(Icons.access_time_outlined),
                   title: const Text('Nombre Ruta'),
                   onTap: () {
-                    //
+                    //a otra pantalla
+                    //1era OPCION****************************
+                    // final route = MaterialPageRoute(
+                    //     builder: (context) => const Listview1Screen());
+                    // Navigator.push(context, route);
+                    //2da OPCION*****************************
+                    Navigator.pushNamed(context, 'card');
                   },
                 ),
             separatorBuilder: (_, __) => const Divider(),
