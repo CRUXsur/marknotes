72fa0a04d8662f2de5a7abb7ac58bfb8bb92e291
CRUXsur <marcojtorrico@hotmail.com>
2022-02-02 15:08:37 -0400

    HomeScreen y rutas iniciales

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
-//import 'package:marknotes/screens/listview1_screen.dart';
-import 'package:marknotes/screens/listview2_screen.dart';
+import 'package:marknotes/screens/screens.dart';
 
 void main() => runApp(const MyApp());
 
@@ -10,10 +9,18 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
         debugShowCheckedModeBanner: false,
         title: 'Material App',
         //home: Listview1Screen());
-        home: Listview2Screen());
+        //home: const Listview2Screen(),
+        initialRoute: 'home',
+        routes: {
+          'home': (BuildContext context) => const HomeScreen(),
+          'listview1': (BuildContext context) => const Listview1Screen(),
+          'listview2': (BuildContext context) => const Listview2Screen(),
+          'alert': (BuildContext context) => const AlertScreen(),
+          'card': (BuildContext context) => const CardScreen()
+        });
   }
 }
---
+++ lib/screens/screens.dart
@@ -0,0 +1,5 @@
+export 'package:marknotes/screens/alert_screen.dart';
+export 'package:marknotes/screens/card_screen.dart';
+export 'package:marknotes/screens/home_screen.dart';
+export 'package:marknotes/screens/listview1_screen.dart';
+export 'package:marknotes/screens/listview2_screen.dart';
