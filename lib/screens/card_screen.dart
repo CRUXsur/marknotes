3d392734b32407ef276c0448ef54f85784858308
CRUXsur <marcojtorrico@hotmail.com>
2022-02-04 10:35:17 -0400

    Card Widget

--- lib/screens/card_screen.dart
+++ lib/screens/card_screen.dart
@@ -1,14 +1,37 @@
 import 'package:flutter/material.dart';
 
+import 'package:marknotes/theme/app_theme.dart';
+
 class CardScreen extends StatelessWidget {
   const CardScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('CardScreen'),
-      ),
-    );
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Card Widget'),
+        ),
+        body: ListView(
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+          children: [
+            //
+            //
+            Card(
+              child: Column(
+                children: const [
+                  ListTile(
+                    leading: Icon(Icons.photo_album_outlined,
+                        color: AppTheme.primary),
+                    title: Text('Soy un titulo'),
+                    subtitle: Text(
+                        'Sint qui quis aute veniam id duis commodo ullamco voluptate minim ea eu. Voluptate fugiat et minim officia. Magna magna ipsum dolor labore officia. Et ut tempor officia excepteur non pariatur sint id cupidatat amet occaecat.'),
+                  )
+                ],
+              ),
+            )
+            //
+            //
+          ],
+        ));
   }
 }
