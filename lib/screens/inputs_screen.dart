c784d648be972a6fb7a9b6a82df8577fda888ea5
CRUXsur <marcojtorrico@hotmail.com>
2022-02-09 19:53:09 -0400

    TextFormFields - Personalizacion

--- lib/screens/inputs_screen.dart
+++ lib/screens/inputs_screen.dart
@@ -14,14 +14,44 @@ class InputsScreen extends StatelessWidget {
             padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
             child: Column(
               children: [
+                //
+                //
                 TextFormField(
-                    autofocus: true, //
+                    autofocus: false, //
                     initialValue: '', //
                     textCapitalization: TextCapitalization.words,
                     onChanged: (value) {
                       //
                       print('value: $value');
-                    })
+                    },
+                    //validaciones
+                    validator: (value) {
+                      if (value == null) return 'Este campo es requerido';
+                      return value.length < 3 ? 'Minimo de 3 letras' : null;
+                    },
+                    autovalidateMode: AutovalidateMode.onUserInteraction,
+                    decoration: const InputDecoration(
+                      hintText: 'Nombre del usuario',
+                      labelText: 'Nombre',
+                      helperText: 'Solo letras',
+                      //counterText:
+                      prefixIcon: Icon(Icons.verified_user_outlined),
+                      suffixIcon: Icon(Icons.group_outlined), //
+                      icon: Icon(Icons.assignment_ind_outlined),
+                      //configurar el color del borde!
+                      focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.green)), //
+                      //
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.only(
+                        bottomLeft: Radius.circular(10),
+                        topRight: Radius.circular(10),
+                      )),
+                    )
+                    //
+                    )
+                //
+                //
               ],
             )),
       ),
