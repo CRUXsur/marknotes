b050f95b5566ecfa7b934688d03c460cbc5fede2
CRUXsur <marcojtorrico@hotmail.com>
2022-02-12 11:00:45 -0400

    InfiniteScroll con Features

--- lib/screens/listview_builder_screen.dart
+++ lib/screens/listview_builder_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:marknotes/theme/app_theme.dart';
+
 class ListViewBuilderScreen extends StatefulWidget {
   const ListViewBuilderScreen({Key? key}) : super(key: key);
 
@@ -10,6 +12,7 @@ class ListViewBuilderScreen extends StatefulWidget {
 class _ListViewBuilderScreenState extends State<ListViewBuilderScreen> {
   final List<int> imagesIds = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   final ScrollController scrollController = ScrollController();
+  bool isLoading = false;
 
   @override
   void initState() {
@@ -20,9 +23,24 @@ class _ListViewBuilderScreenState extends State<ListViewBuilderScreen> {
       //     '${scrollController.position.pixels},${scrollController.position.maxScrollExtent}');
       if ((scrollController.position.pixels) >=
           scrollController.position.maxScrollExtent) {
-        add5();
+        //add5();
+        fetchData();
       }
     });
+  }
+
+//*creo una funcion o un metodo que regersa un Future y se llama  fetchData
+//* parahacer un tipo de espera mientras los tengo los resultados
+  Future fetchData() async {
+    //
+    if (isLoading) return;
+    isLoading = true; //hace algun tipo de interacccion en mi widget
+    //**espero a que esto resuelva, espero 3 seg
+    await Future.delayed(const Duration(seconds: 3));
+    //********
+    add5();
+    //**ya deje de caragar
+    isLoading = false;
   }
 
   void add5() {
@@ -33,32 +51,71 @@ class _ListViewBuilderScreenState extends State<ListViewBuilderScreen> {
 
   @override
   Widget build(BuildContext context) {
+    //? PARA TENER INFORMACION DEL DISPOSITIVO....
+    //? size sabe el ancho y alto de la pantalla, actual...
+    final size = MediaQuery.of(context).size;
+
     return Scaffold(
       backgroundColor: Colors.black,
       body: MediaQuery.removePadding(
         context: context,
         removeTop: true,
         removeBottom: true,
-        child: ListView.builder(
-          //** para ver el efecto de IOS al final del scroll
-          physics: const BouncingScrollPhysics(),
-          controller: scrollController,
-          itemCount: imagesIds.length,
-          itemBuilder: (BuildContext context, int index) {
-            return FadeInImage(
-                //?para que al cargar la imagen no salte!
-                width: double.infinity,
-                height: 300,
-                //?-----
-                //? quiero todas imagenes pegadas
-                fit: BoxFit.cover,
-                //?-----
-                placeholder: const AssetImage('assets/jar-loading.gif'),
-                image: NetworkImage(
-                    'https://picsum.photos/500/300?image=${imagesIds[index]}'));
-          },
+        //!Stack es para poner un widget encima de otro
+        //!similares son Column, Row y Stack
+        //!wrap  with Column => wrap with Stack
+        child: Stack(
+          children: [
+            ListView.builder(
+              //** para ver el efecto de IOS al final del scroll
+              physics: const BouncingScrollPhysics(),
+              controller: scrollController,
+              itemCount: imagesIds.length,
+              itemBuilder: (BuildContext context, int index) {
+                return FadeInImage(
+                    //?para que al cargar la imagen no salte!
+                    width: double.infinity,
+                    height: 300,
+                    //?-----
+                    //? quiero todas imagenes pegadas
+                    fit: BoxFit.cover,
+                    //?-----
+                    placeholder: const AssetImage('assets/jar-loading.gif'),
+                    image: NetworkImage(
+                        'https://picsum.photos/500/300?image=${imagesIds[index]}'));
+              },
+            ),
+
+            Positioned(
+              bottom: 40,
+              left: size.width * 0.5 - 30, //*-30,ancho del widget es 60
+              child: const _LoadingIcon(),
+            ), //
+          ],
         ),
       ),
     );
+  }
+}
+
+class _LoadingIcon extends StatelessWidget {
+  const _LoadingIcon({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        padding: const EdgeInsets.all(10),
+        //color: Colors.white,
+        height: 60,
+        width: 60,
+        decoration: BoxDecoration(
+            color: Colors.white.withOpacity(0.9), //
+            shape: BoxShape.circle //
+            ),
+        child: const CircularProgressIndicator(
+          color: AppTheme.primary,
+        ));
   }
 }
